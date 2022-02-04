5b0ea1d947984e3c48dd339135cd96f9b1bc9663
Kade <98916709+KImSuperman@users.noreply.github.com>
2022-02-04 17:34:42 +0000

    Project Files
    Adding main project files

---
+++ PrjV1/face.py
@@ -0,0 +1,125 @@
+import cv2
+
+face_cascade_front = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt.xml')
+face_cascade_profile = cv2.CascadeClassifier('data/haarcascades/haarcascade_profileface.xml')
+face_cascade_extended = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalcatface_extended.xml')
+face_cascade_eye = cv2.CascadeClassifier('data/haarcascades/haarcascade_eye.xml')
+face_cascade_glasses = cv2.CascadeClassifier('data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+face_cascade_leftEye = cv2.CascadeClassifier('data/haarcascades/haarcascade_lefteye_2splits.xml')
+face_cascade_rightEye = cv2.CascadeClassifier('data/haarcascades/haarcascade_righteye_2splits.xml')
+	
+body_cascade_full = cv2.CascadeClassifier('data/haarcascades/haarcascade_fullbody.xml')
+
+# frameWidth = 640
+# frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+
+# cap.set(3, frameWidth)
+# cap.set(4, frameHeight)
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces_eye = face_cascade_eye.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_eye:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = -1
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (255, 0, 0) #BGR 0-255
+		stroke = 2
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+	faces_leftEye = face_cascade_leftEye.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_leftEye:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = -1
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (255, 0, 0) #BGR 0-255
+		stroke = 1
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+	faces_rightEye = face_cascade_rightEye.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_rightEye:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = -1
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (255, 0, 0) #BGR 0-255
+		stroke = 1
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)	
+		
+	
+	faces_front = face_cascade_front.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_front:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = -1
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+		
+		color = (0, 255, 0) #BGR 0-255
+		stroke = 2
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+	faces_profile = face_cascade_profile.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_profile:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = -1
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (0, 255, 0) #BGR 0-255
+		stroke = 2
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+	faces_extended = face_cascade_extended.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in faces_extended:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = 2
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (0, 255, 0) #BGR 0-255
+		stroke = -1
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+	bodies_full = body_cascade_full.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x, y, w, h) in bodies_full:
+		
+		color = (0, 0, 0) #BGR 0-255
+		stroke = 2
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
+		color = (0, 0, 255) #BGR 0-255
+		stroke = -1
+		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)	
+
+	# Display the resulting frame
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(20) & 0xFF == ord('q'):
+		break
+
+# When everything done, reease the capture
+cap.release()
+cv2.destroyAllWindows()
+
