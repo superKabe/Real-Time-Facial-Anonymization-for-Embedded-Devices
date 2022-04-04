aa0c72282f3ac550d46ddd1464753489f472a638
Mark-Slat <100161972+Mark-Slat@users.noreply.github.com>
2022-04-04 02:38:44 +0100

    Main app also calls data_loader

---
+++ prjV2/camera.py
@@ -0,0 +1,39 @@
+import warnings
+warnings.filterwarnings('ignore', message='GStreamer')
+
+import cv2
+import time
+
+
+print("Please look into the camera")
+fullName = input('Enter your first name: ')
+print("Please look into the camera and press SPACE to capture")
+
+fullName = str(fullName).replace(" ", "-")
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("Camera")
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("Camera", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "Project/testing/3/images/{}.png".format(fullName)
+        cv2.imwrite(img_name, frame)
+        break
+
+
+time.sleep(3)
+
+cam.release()
+cv2.destroyAllWindows()
---
+++ prjV2/data_loader.py
@@ -0,0 +1,20 @@
+import face_recognition
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+known_face_names = []
+known_face_encodings = []
+
+for root, dirs, files in os.walk(image_dir):
+	for file in files:
+		if file.endswith("jpg") or file.endswith("png"):
+			path = os.path.join(root, file)
+			
+			image = face_recognition.load_image_file(path)
+			image_encode = face_recognition.face_encodings(image)[0]
+			known_face_encodings.append(image_encode)
+
+			lables = (os.path.basename(path).replace(".jpg", "").replace(".png", "").replace("-", " "))
+			known_face_names.append(lables)
